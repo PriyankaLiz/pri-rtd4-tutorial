0b2e226641245a06fe525ad749bc02a0c04818da
PriyankaLiz <78859858+PriyankaLiz@users.noreply.github.com>
2022-11-01 16:32:54 +0530

    Added files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,11 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+html_static_path = ['.static']
+
+# Display tables with no horizontal scrollbar
+def setup(app):
+    app.add_css_file('custom.css')
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
