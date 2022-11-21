e448c18095f4ba58bf14d49032a9fbead22cf600
PriyankaLiz <78859858+PriyankaLiz@users.noreply.github.com>
2022-11-21 17:14:34 +0530

    Added raw-html role

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,11 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+rst_prolog = """
+.. role:: raw-html(raw)
+   :format: html
+"""
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
